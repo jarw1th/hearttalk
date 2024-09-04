5870b2108984e44ba0eb2ebb7a8962585aa7f3f7
jarw1th <parastaev31@gmail.com>
2024-09-04 21:36:28 +0300

    Init

---
+++ hearttalk/hearttalk/hearttalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  hearttalkApp.swift
+//  hearttalk
+//
+//  Created by Руслан Парастаев on 04.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct hearttalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
